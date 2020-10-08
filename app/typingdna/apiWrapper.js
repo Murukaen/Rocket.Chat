caaf8983f99468223d90bd2b9c2d4cf997c7bc3a
Octavian <octavian.v@typingdna.com>
2020-10-08 16:33:52 +0300

    WOrking fraud detection

--- app/typingdna/apiWrapper.js
+++ app/typingdna/apiWrapper.js
@@ -1,14 +1,27 @@
 import { HTTP } from 'meteor/http';
 
 const makeVerify = (user, pattern) => {
-    const { data: { net_score : netScore } } = HTTP.post(`https://apidev.typingdna.com/verify/${user}`, {
+    const { data: {net_score : netScore} } = HTTP.post(`https://apidev.typingdna.com/verify/${user}`, {
         data: {tp: pattern},
         auth: '89fe60f493563dd67115f841f71a83e4:b3ad0256c82ea9a0f5b3639ccdf9d255',
     });
 
-    console.log('netScore', netScore);
+    return netScore;
 };
+
+const mapUser = username => {
+    switch(username) {
+        case 'Octa':
+            return 'test1@typingdna.com';
+    }
+
+    return '';
+}
+
+const scoreThreshold = 50;
 
 module.exports = {
     makeVerify,
+    mapUser,
+    scoreThreshold,
 }
